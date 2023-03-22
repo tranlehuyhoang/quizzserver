7d5f2f7f798376ed6d0ec2879871ddd25fdbd800
Huyh Huyhu <2509roblox@gmail.com>
2023-03-22 08:29:43 +0700

    fa

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 config()
 app.use('/api', router)
 
-const port = process.env.PORT || 5000;
+const port =  5000;
 app.get('/', (req, res) => {
     try {
         res.json('get câu hỏi')
